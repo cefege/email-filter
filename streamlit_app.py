146daf704cb5719b18c12067d470d8c5b4dccd01
cefege <mateiasmihaiandrei@gmail.com>
2023-05-07 00:26:40 +0300

    filter by domain then name

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import streamlit as st
 
-def process_emails(email_list):
 
+def process_emails(email_list):
     # Remove any elements that do not contain '@'
-    email_list = [email for email in email_list.split('\n') if '@' in email]
+    email_list = [email for email in email_list.split("\n") if "@" in email]
 
     # convert all emails to lowercase
     email_list = [email.lower() for email in email_list]
@@ -13,37 +13,41 @@ def process_emails(email_list):
     email_list = list(set(email_list))
 
     # Split the email list into a list of tuples containing the name and domain
-    email_tuples = [(email.split('@')[0], email.split('@')[1]) for email in email_list]
+    email_tuples = [(email.split("@")[0], email.split("@")[1]) for email in email_list]
 
     # Convert the list of tuples into a pandas dataframe
-    df = pd.DataFrame(email_tuples, columns=['name', 'domain'])
+    df = pd.DataFrame(email_tuples, columns=["name", "domain"])
 
-    # Sort the dataframe by the domain column
-    df = df.sort_values('domain')
+    # Sort the dataframe by the domain column and then by the name column
+    df = df.sort_values(["domain", "name"])
 
     # Combine the name and domain columns with '@' separator
-    df['email'] = df['name'] + '@' + df['domain']
+    df["email"] = df["name"] + "@" + df["domain"]
 
     # Select only the email column for output
-    output_df = df[['email']]
+    output_df = df[["email"]]
 
     # Return the output dataframe
     return output_df
 
+
 def app():
     # Define a text input for the list of emails
-    email_list = st.text_area('Enter a list of emails (one email per line). All duplicates will be removed and the list will be sorted by email domain in alphabateical order.')
+    email_list = st.text_area(
+        "Enter a list of emails (one email per line). All duplicates will be removed and the list will be sorted by email domain in alphabateical order."
+    )
 
     # Add a "Submit" button to trigger the processing of the emails
-    if st.button('Submit'):
+    if st.button("Submit"):
         # Process the email list using the process_emails function
         output_df = process_emails(email_list)
 
         # convert column "email" to a string split by \n
-        output_df = output_df['email'].str.cat(sep='\n')
+        output_df = output_df["email"].str.cat(sep="\n")
 
         # Write the output dataframe to the Streamlit app
         st.code(output_df)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app()
